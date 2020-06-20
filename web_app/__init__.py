9608364cc1e973e7e09f7f00bc8ee9980cc04d01
Afroman <makoanoble@gmail.com>
2020-06-20 00:03:03 -0600

    add procfile and update __init__.py

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,15 +1,13 @@
 # web_app/__init__.py
 from flask import Flask
 import os
-from sqlalchemy import create_engine
-import psycopg2
-from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
 load_dotenv()
 
 
-from web_app.models import db, migrate
+from web_app.models import db
 from web_app.routes.insert_routes import insert_routes
+from flask_migrate import Migrate
 
 DATABASE_URL = os.getenv("DB_URL")
 
@@ -19,9 +17,9 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     db.init_app(app)
-    migrate.init_app(app, db)
+    # migrate.init_app(app, db)
+    migrate = Migrate(app, db)
 
-    # app.register_blueprint(home_routes)
     app.register_blueprint(insert_routes)
 
     return app
