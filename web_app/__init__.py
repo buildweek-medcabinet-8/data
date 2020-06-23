b22d7c1682bfd744d8ba0254f9824a7e7b64dc26
Afroman <makoanoble@gmail.com>
2020-06-22 20:43:45 -0600

    change init.py to have a method to create the app, and create procfile to tell heroku how to run the app

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,11 +2,19 @@ from flask import Flask
 from web_app.routes.home_routes import home_routes
 from web_app.routes.insert_routes import insert_routes
 
-app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "temporary secret key"
+def create_app():
 
-app.register_blueprint(home_routes)
-app.register_blueprint(insert_routes)
+    app = Flask(__name__)
 
-app.run(debug=True)
+    app.config["SECRET_KEY"] = "temporary secret key"
+
+    app.register_blueprint(home_routes)
+    app.register_blueprint(insert_routes)
+
+    return app
+
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
