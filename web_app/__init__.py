30f610fc50aab29bf12d57796cf6db004baa52af
Afroman <makoanoble@gmail.com>
2020-06-23 22:23:02 -0500

    add db and migrate to init

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from web_app.routes.home_routes import home_routes
 from web_app.routes.insert_routes import insert_routes
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
 
+db = SQLAlchemy()
+
+migrate = Migrate()
 
 def create_app():
 
     app = Flask(__name__)
-
     app.config["SECRET_KEY"] = "temporary secret key"
+
+    # app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+
+    db.init_app(app)
+    # # migrate.init_app(app, db)
+    migrate = Migrate(app, db)
 
     app.register_blueprint(home_routes)
     app.register_blueprint(insert_routes)
